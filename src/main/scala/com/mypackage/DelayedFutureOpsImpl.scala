267b156a5d4bdd182436394cb456ee3029ab05e4
Ilya Kondratyuk <ikondratyuk@systematica-consulting.ru>
2024-05-28 11:42:31 +0700

    fix homework6 again

--- src/main/scala/com/mypackage/DelayedFutureOpsImpl.scala
+++ src/main/scala/com/mypackage/DelayedFutureOpsImpl.scala
@@ -6,16 +6,12 @@ import scala.util.{Failure, Success, Try}
 object DelayedFutureOpsImpl extends DelayedFutureOps {
 
   def sequence[A](list: List[DelayedFuture[A]])(using ExecutionContext): DelayedFuture[List[A]] = () => {
-    def helper(remaining: List[DelayedFuture[A]], acc: List[A]): Future[List[A]] = {
-      remaining match {
-        case Nil => Future.successful(acc)
-        case head :: tail =>
-          head().flatMap { result =>
-            helper(tail, acc :+ result)
-          }
-      }
+    list.foldRight(Future.successful(List.empty[A])) { (delayed, accFuture) =>
+      for {
+        acc <- accFuture
+        result <- delayed()
+      } yield result :: acc
     }
-    helper(list, Nil)
   }
 
   def seqPar[A](list: List[DelayedFuture[A]])(using ExecutionContext): DelayedFuture[(List[Throwable], List[A])] = () => {
@@ -26,7 +22,7 @@ object DelayedFutureOpsImpl extends DelayedFutureOps {
           head().transform(result => Success(Try(result))).flatMap {
             case Success(Success(value)) => helper(tail, errors, successes :+ value)
             case Success(Failure(ex)) => helper(tail, errors :+ ex, successes)
-            case Failure(ex) => helper(tail, errors :+ ex, successes) // to handle unexpected failure in transform
+            case Failure(ex) => helper(tail, errors :+ ex, successes) // обработка неожиданных ошибок в transform
           }
       }
     }
@@ -34,32 +30,21 @@ object DelayedFutureOpsImpl extends DelayedFutureOps {
   }
 
   def traverse[A, B](list: List[A])(f: A => DelayedFuture[B])(using ExecutionContext): DelayedFuture[List[B]] = () => {
-    def helper(remaining: List[A], acc: List[B]): Future[List[B]] = {
-      remaining match {
-        case Nil => Future.successful(acc)
-        case head :: tail =>
-          f(head)().flatMap { result =>
-            helper(tail, acc :+ result)
-          }
-      }
+    list.foldRight(Future.successful(List.empty[B])) { (a, accFuture) =>
+      for {
+        acc <- accFuture
+        result <- f(a)()
+      } yield result :: acc
     }
-    helper(list, Nil)
   }
 
   def batchTraverse[A](in: List[A], batchSize: Int, futures: DelayedFuture[A])(using ExecutionContext): DelayedFuture[List[A]] = () => {
     val batches = in.grouped(batchSize).toList
 
     def processBatch(acc: Future[List[A]], batch: List[A]): Future[List[A]] = {
-      def helper(remaining: List[A], accBatch: List[A]): Future[List[A]] = {
-        remaining match {
-          case Nil => Future.successful(accBatch)
-          case head :: tail =>
-            futures().flatMap { result =>
-              helper(tail, accBatch :+ result)
-            }
-        }
+      acc.flatMap { lst =>
+        traverse(batch)(_ => futures).apply().map(lst ++ _)
       }
-      acc.flatMap(lst => helper(batch, Nil).map(lst ++ _))
     }
 
     batches.foldLeft(Future.successful(List.empty[A]))(processBatch)
@@ -74,7 +59,7 @@ object DelayedFutureOpsImpl extends DelayedFutureOps {
           }
         }
       }.flatMap { result =>
-        if (result == t) Future.failed(new RuntimeException("All transformations failed"))
+        if (result == t) Future.failed(new RuntimeException("Все трансформации не удались"))
         else Future.successful(result)
       }
     }
